1666997eec134a14feb4291acf079b927523f7ae
vik37 <59177404+vik37@users.noreply.github.com>
2021-01-28 22:30:28 +0100

    PizzaApp_DemoMVC

---
+++ ASP.NET.MVC.MODEL.DEMO/ASP.NET.MVC.MODEL.DEMO/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASP.NET.MVC.MODEL.DEMO.Models;
+using ASP.NET.MVC.MODEL.DEMO.Models.Domain;
+using ASP.NET.MVC.MODEL.DEMO.Models.ViewModels;
+
+namespace ASP.NET.MVC.MODEL.DEMO.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            User person = new User() { FirstName = "Bob", LastName = "Bobsky", Address = "Bob Street", Phone = 0800234234 };
+            Order order = new Order() { Id = 12, User = person, Pizza = "Kapri", Price = 10.5 };
+            
+            ViewBag.Order = "ORDER";
+            return View(order);
+        }
+        public IActionResult Details()
+        {
+            User person = new User() { FirstName = "Bob", LastName = "Bobsky", Address = "Bob Street", Phone = 0800234234 };
+            Order order = new Order() { Id = 12, User = person, Pizza = "Kapri", Price = 10.5 };
+            OrderDetailsVM orderDetails = new OrderDetailsVM()
+            {
+                Id = order.Id,
+                FullName = $"{person.FirstName} {person.LastName}",
+                Address = person.Address,
+                Contact = person.Phone,
+                Pizza = order.Pizza,
+                Price = order.Price
+            };
+            return View(orderDetails);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+       
+    }
+}
---
+++ ASP.NET.MVC.MODEL.DEMO/ASP.NET.MVC.MODEL.DEMO/Models/ViewModels/OrderDetailsVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET.MVC.MODEL.DEMO.Models.ViewModels
+{
+    public class OrderDetailsVM
+    {
+		public int Id { get; set; }
+		public string FullName { get; set; }
+		public string Address { get; set; }
+		public long Contact { get; set; }
+		public string Pizza { get; set; }
+		public double Price { get; set; }
+	}
+}
